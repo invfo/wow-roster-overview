d65d3625b776040aebe9ad3564b491b0f614cd37
Darya Talanina <dtalanina@gmail.com>
2017-05-24 22:46:06 +0200

    Add basic backed using 'pyramid'

--- server.py
+++ server.py
@@ -1,8 +1,35 @@
-import SimpleHTTPServer
-import SocketServer
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
 
-PORT = 8000
+import requests
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-httpd.serve_forever()
+# get yours at https://dev.battle.net
+KEY = ''
+
+def manage_player(request):
+
+    server = request.matchdict['server']
+    player = request.matchdict['player']
+
+    # send request to Blizzard server
+    url = 'https://eu.api.battle.net/wow/character/%s/%s' \
+            '?fields=items&locale=en_GB&apikey=%s' \
+            % (server, player, KEY)
+
+    r = requests.get(url)
+
+    return Response('%s' % r.json())
+
+
+if __name__ == '__main__':
+    config = Configurator()
+
+    config.add_route('player', '/player/{server}/{player}')
+    config.add_view(manage_player, route_name='player')
+
+    config.add_static_view(name='/', path='./static')
+
+    app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 8080, app)
+    server.serve_forever()
