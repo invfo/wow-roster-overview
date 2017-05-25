46d4b6ee41ec285dbf37657ea5bb8f5891f8f03f
Darya Talanina <dtalanina@gmail.com>
2017-05-25 15:08:04 +0200

    Add function to get active specialization

--- app.py
+++ app.py
@@ -8,10 +8,24 @@ import requests
 # get yours at https://dev.battle.net
 KEY = ''
 
+
+def get_active_spec(server, player, key):
+    url = 'https://eu.api.battle.net/wow/character/' \
+            '%s/%s?fields=talents&locale=en_GB&apikey=%s' \
+            % (server, player, key)
+    r = requests.get(url)
+    talent_info = r.json()['talents']
+    for e in talent_info:
+        if 'spec' in e:
+            if e.get('selected', None):
+                return e['spec']['name']
+
+
 def manage_player(request):
 
     server = request.matchdict['server']
     player = request.matchdict['player']
+    required_spec = request.matchdict['spec']
 
     # send request to Blizzard server
     url = 'https://eu.api.battle.net/wow/character/%s/%s' \
@@ -19,6 +33,7 @@ def manage_player(request):
             % (server, player, KEY)
 
     r = requests.get(url)
+    active_spec = get_active_spec(server, player, KEY)
     return r.json()
 
 def make_app():
